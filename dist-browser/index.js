5027daea03653585af2af10edf068d7b0e162c8f
edwinparejas <edwpar@hotmail.com>
2022-03-17 15:57:59 -0500

    first commit

---
+++ dist-browser/index.js
@@ -0,0 +1,26 @@
+/**
+ * Adds two numbers
+ * @param a Number one
+ * @param b Number two
+ * @returns Result
+ */
+var add = function (a, b) { return a + b; };
+/**
+ * Substracts two numbers
+ * @param a Number one
+ * @param b Number two
+ * @returns Result
+ */
+var substract = function (a, b) { return a - b; };
+/**
+ * Multiplies two numbers
+ * @param a Number one
+ * @param b Number two
+ * @returns Result
+ */
+var multiply = function (a, b) { return a * b; };
+export default {
+    add: add,
+    substract: substract,
+    multiply: multiply
+};
---
+++ src/index.ts
@@ -0,0 +1,29 @@
+/**
+ * Adds two numbers
+ * @param a Number one
+ * @param b Number two
+ * @returns Result
+ */
+export const add = (a: number, b: number) => a + b;
+
+/**
+ * Substracts two numbers
+ * @param a Number one
+ * @param b Number two
+ * @returns Result
+ */
+export const substract = (a: number, b: number) => a - b;
+
+/**
+ * Multiplies two numbers
+ * @param a Number one
+ * @param b Number two
+ * @returns Result
+ */
+export const multiply = (a: number, b: number) => a * b;
+
+// export default {
+// 	add,
+// 	substract,
+// 	multiply
+// };
